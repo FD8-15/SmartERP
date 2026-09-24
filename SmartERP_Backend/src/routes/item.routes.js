0bb859f362b98df8cd1742eb8f91a441872ff398
Raj Naik <rajrnaik12@gmail.com>
2026-09-24 17:52:35 +0530

    Added routes for items

---
+++ SmartERP_Backend/src/routes/item.routes.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { createItem, getItems, getOneItem, update } from "../controllers/item.controller.js";
+import { auth } from "../middlewares/auth.middleware.js";
+import { company_access } from "../middlewares/company_access.middleware.js";
+import { checkRole } from "../middlewares/checkRole.middleware.js";
+
+const router = Router();
+
+router.post("/:company_id", auth, company_access, checkRole("owner", "manager"), createItem);
+router.get("/:company_id", auth, company_access, getItems);
+router.get("/:company_id/:item_id", auth, company_access, getOneItem);
+router.patch("/:company_id/:item_id", auth, company_access, checkRole("owner", "manager"), update);
+
+export default router;
